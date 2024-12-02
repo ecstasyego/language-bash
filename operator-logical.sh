d4f9b4d059ffa81237cbdc375bc0f6c7f4583218
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-12-02 16:29:24 +0900

    Update operator-logical.sh

--- operator-logical.sh
+++ operator-logical.sh
@@ -7,7 +7,17 @@ then
     echo "Logical Operator: &&"
 fi
 
+if [ $positive_variable -gt 0 ] && [ $negative_variable -lt 0 ]
+then
+    echo "Logical Operator: &&"
+fi
+
 if [ $positive_variable>0 -a $negative_variable<0 ]
+then
+    echo "Logical Operator: -a"
+fi
+
+if [ $positive_variable -gt 0 -a $negative_variable -lt 0 ]
 then
     echo "Logical Operator: -a"
 fi
@@ -18,7 +28,17 @@ then
     echo "Logical Operator: ||"
 fi
 
+if [ $positive_variable -gt 0 ] || [ $negative_variable -eq 0 ]
+then
+    echo "Logical Operator: ||"
+fi
+
 if [ $positive_variable>0 -o $negative_variable=0 ]
+then
+    echo "Logical Operator: -o"
+fi
+
+if [ $positive_variable -gt 0 -o $negative_variable -eq 0 ]
 then
     echo "Logical Operator: -o"
 fi
